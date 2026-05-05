956a0b29d2cda1e7620109e124f82deca0f25eb3
Celine86 <celine.obringer@live.fr>
2026-05-05 17:33:24 +0200

    news sound ?

--- crackTheEgg.service.worker.js
+++ crackTheEgg.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777992709|7759508101';
+const CACHE_VERSION = '1777993034|8084748670';
 /** @type {string} */
 const CACHE_PREFIX = 'crackTheEgg-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
